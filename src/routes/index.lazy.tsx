3e3671e992a3810d5ff8e56f58051add16ade329
Liangdi <ligsnf@gmail.com>
2024-11-30 14:26:15 +1100

    styling updates - remove unecessary css - add theme and enable css variables

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,4 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button"
+import { Switch } from "@/components/ui/switch"
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -8,6 +10,8 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button>Button</Button>
+      <Switch />
     </div>
   )
 }
